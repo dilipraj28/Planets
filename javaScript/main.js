6ddce3879c469488c90bc586c7e29c38661f991d
dilipraj <s.champ2360@gmail.com>
2021-09-13 21:23:45 +0530

    planet

---
+++ javaScript/main.js
@@ -0,0 +1,355 @@
+// -----------------------------...........MERCURY........-----------------------------------
+
+
+function mercury_txt_1() {
+    document.getElementById('planet_image').src = 'mercury.png'
+    document.getElementById('mercury_text').innerHTML = `<h1>MERCURY</h1>
+                <br>
+                <p>Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the
+                    Sun
+                    takes
+                    87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets
+                    in
+                    the
+                    Solar System, and is a rocky body like Earth.</p>
+                <br>`
+    // console.log("clicked");
+}
+
+
+function mercury_txt_2() {
+    document.getElementById('planet_image').src = "mercury-2.png"
+    document.getElementById('mercury_text').innerHTML = `<h1>MERCURY</h1>
+                <br>
+                <p>Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide
+                    outer core
+                    layer, a deeper liquid core layer, and a solid inner core. The planet's density is the
+                    second highest in
+                    the Solar System at 5.427 g/cm3 , Earth's density.</p>
+                <br>`
+}
+
+
+
+function mercury_txt_3() {
+    document.getElementById('planet_image').src = 'mercury-4.png'
+    document.getElementById('mercury_text').innerHTML = `<h1>MERCURY</h1>
+                <br>
+                <p>Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains
+                    and
+                    heavy cratering, indicating that it has been geologically inactive for billions of years. It is more
+                    heterogeneous than either Mars's or the Moon’s.</p>
+                <br>`
+}
+
+
+// -----------------------------...........VENUS........-----------------------------------
+
+// function venus_main_page(){
+//     document.getElementById('planet_image').src = 'venus.png'
+// }
+
+// venus_button_1.onclick = venus_txt_1;
+
+function venus_txt_1() {
+    document.getElementById('planet_image').src = 'venus-1.png'
+    document.getElementById('venus_text').innerHTML = `<h1>VENUS</h1>
+                <br>
+                <p>Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As
+                    the
+                    brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be, on
+                    rare
+                    occasions, visible to the naked eye in broad daylight.</p>
+                <br>`
+    // console.log("clicked");
+}
+// venus_button_2.onclick = venus_txt_2;
+
+function venus_txt_2() {
+    document.getElementById('planet_image').src = 'venus-2.png'
+    document.getElementById('venus_text').innerHTML = `<h1>VENUS</h1>
+            <br>
+            <p>The similarity in size and density between Venus and Earth suggests they share a similar internal
+                structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least
+                partially liquid because the two planets have been cooling at same rate.</p>
+            <br>`
+}
+
+// venus_button_3.onclick = venus_txt_3;
+
+function venus_txt_3() {
+    document.getElementById('planet_image').src = 'venus-5.png'
+    document.getElementById('venus_text').innerHTML = `<h1>VENUS</h1>
+            <br>
+            <p>Much of the Venusian surface appears to have been shaped by volcanic activity.
+                many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only
+                volcanic complex of this size on Earth is the Big Island of Hawaii.</p>
+            <br>`
+}
+
+
+// -----------------------------...........EARTH........-----------------------------------
+
+
+
+// earth_button_1.onclick = earth_txt_1;
+
+function earth_txt_1() {
+    document.getElementById('planet_image').src = 'earth.png'
+
+    document.getElementById('earth_text').innerHTML = `<h1>EARTH</h1>
+                <br>
+                <p>Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is
+                    land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical and
+                    geological history have allowed life to evolve and thrive.</p>
+                <br>`
+    // console.log("clicked");
+}
+// earth_button_2.onclick = earth_txt_2;
+
+function earth_txt_2() {
+    document.getElementById('planet_image').src = 'earth-2.png'
+
+    document.getElementById('earth_text').innerHTML = `<h1>EARTH</h1>
+            <br>
+            <p>The total surface area of Earth is about 510 million km2. The continental crust consists of lower density
+                material such as rocks granite and andesite. Less common is basalt, a denser volcanic rock
+                that is the primary constituent of the ocean floors.</p>
+            <br>`
+}
+
+// earth_button_3.onclick = earth_txt_3;
+
+function earth_txt_3() {
+    document.getElementById('planet_image').src = 'earth-4.png'
+
+    document.getElementById('earth_text').innerHTML = `<h1>EARTH</h1>
+            <br>
+            <p>Much of the Venusian surface appears to have been shaped by volcanic activity.
+                many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only
+                volcanic complex of this size on Earth is the Big Island of Hawaii.</p>
+            <br>`
+}
+
+
+// -----------------------------...........MARS........-----------------------------------
+
+
+
+
+// mars_button_1.onclick = mars_txt_1;
+
+function mars_txt_1() {
+    document.getElementById('planet_image').src = 'mars.png'
+
+    document.getElementById('mars_text').innerHTML = `<h1>MARS</h1>
+                <br>
+                <p>Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being
+                    larger
+                    than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred
+                    to as
+                    the "Red Planet".</p>
+                <br>`
+    // console.log("clicked");
+}
+// mars_button_2.onclick = mars_txt_2;
+
+function mars_txt_2() {
+    document.getElementById('planet_image').src = 'mars-2.png'
+
+    document.getElementById('mars_text').innerHTML = `<h1>MARS</h1>
+            <br>
+            <p>Like Earth, Mars has differentiated into a dense metallic core overlaid by less dense materials.
+                Scientists initially determined that the core is at least partially liquid. A core consisting primarily of iron and nickel with about 16–17% sulfur.</p>
+            <br>`
+}
+
+// mars_button_3.onclick = mars_txt_3;
+
+function mars_txt_3() {
+    document.getElementById('planet_image').src = 'mars-3.png'
+
+    document.getElementById('mars_text').innerHTML = `<h1>MARS</h1>
+            <br>
+            <p>Mars is a terrestrial planet whose surface consists of minerals containing silicon and oxygen, metals,
+                and other elements that typically make up rock. The surface is primarily tholeiitic basalt,
+                although parts are more silica-rich than typical basalt.</p>
+            <br>`
+}
+
+
+
+// -------------------------------.........SATURN............------------------------------
+
+
+function saturn_txt_1() {
+    document.getElementById('planet_image').src = 'saturn.png'
+
+    document.getElementById('saturn_text').innerHTML = `<h1>SATURN</h1>
+                <br>
+                <p>Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It
+                    is a gas giant with an average radius of about nine and a half times that of Earth. It only has
+                    one-eighth the average density of Earth.</p>
+                <br>`
+    // console.log("clicked");
+}
+// mars_button_2.onclick = mars_txt_2;
+
+function saturn_txt_2() {
+    document.getElementById('planet_image').src = 'saturn-2.png'
+
+    document.getElementById('saturn_text').innerHTML = `<h1>SATURN</h1>
+            <br>
+            <p>Despite consisting mostly of hydrogen and helium, most of Saturn's mass is not in the gas phase, because hydrogen becomes a non-ideal liquid when the density is above 0.01 g/cm3, which is reached at a radius containing 99.9% of Saturn's mass.</p>
+            <br>`
+}
+
+// mars_button_3.onclick = mars_txt_3;
+
+function saturn_txt_3() {
+    document.getElementById('planet_image').src = 'saturn-3.png'
+
+    document.getElementById('saturn_text').innerHTML = `<h1>SATURN</h1>
+            <br>
+            <p>The outer atmosphere of Saturn contains 96.3% molecular hydrogen and 3.25% helium by volume. The planet's most famous feature is its prominent ring system, which is composed mostly of ice particles with a smaller amount of rocky debris and dust. </p>
+            <br>`
+}
+
+
+// -----------------------------...........JUPITER........-----------------------------------
+
+
+
+
+// jupiter_button_1.onclick = jupiter_txt_1;
+
+function jupiter_txt_1() {
+    document.getElementById('planet_image').src = 'jupiter.png'
+
+    document.getElementById('jupiter_text').innerHTML = `<h1>JUPITER</h1>
+                <br>
+                <p>Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with
+                    a
+                    mass two and a half times that of all the other planets in the Solar System combined, but less than
+                    one-thousandth the mass of the Sun.</p>
+                <br>`
+    // console.log("clicked");
+}
+// jupiter_button_2.onclick = jupiter_txt_2;
+
+function jupiter_txt_2() {
+    document.getElementById('planet_image').src = 'jupiter-2.png'
+
+    document.getElementById('jupiter_text').innerHTML = `<h1>JUPITER</h1>
+            <br>
+            <p>When the Juno arrived in 2016, it found that Jupiter has a very diffuse core.
+                A possible cause is an impact from a planet of about ten Earth masses a few million years after
+                Jupiter's formation, which would have disrupted an originally solid Jovian core.</p>
+            <br>`
+}
+
+// jupiter_button_3.onclick = jupiter_txt_3;
+
+function jupiter_txt_3() {
+    document.getElementById('planet_image').src = 'jupiter-4.png'
+
+    document.getElementById('jupiter_text').innerHTML = `<h1>JUPITER</h1>
+            <br>
+            <p>The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22°
+                south of the equator.It is a gaint planet in the solar It is known to have existed since at least 1831, and possibly since 1665.</p>
+            <br>`
+}
+
+
+
+// -----------------------------...........URANUS........-----------------------------------
+
+
+
+
+// uranus_button_1.onclick = uranus_txt_1;
+
+function uranus_txt_1() {
+    document.getElementById('planet_image').src = 'uranus.png'
+
+    document.getElementById('uranus_text').innerHTML = `<h1>URANUS</h1>
+                <br>
+                <p>Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky,
+                    Uranus
+                    according to Greek mythology. It has the third-largest planetary
+                    radius and fourth-largest planetary mass in the Solar System.</p>
+                <br>`
+    // console.log("clicked");
+}
+// uranus_button_2.onclick = uranus_txt_2;
+
+function uranus_txt_2() {
+    document.getElementById('planet_image').src = 'uranus-2.png'
+
+    document.getElementById('uranus_text').innerHTML = `<h1>URANUS</h1>
+            <br>
+            <p>The standard model of Uranus's structure is that it consists of three layers: a rocky
+                core in the centre, an icy mantle in the middle and an outer gaseous
+                hydrogen/helium envelope. The core is relatively small, with a mass of only 0.55 Earth masses.</p>
+            <br>`
+}
+
+// uranus_button_3.onclick = uranus_txt_3;
+
+function uranus_txt_3() {
+    document.getElementById('planet_image').src = 'uranus-4.png'
+
+    document.getElementById('uranus_text').innerHTML = `<h1>URANUS</h1>
+            <br>
+            <p>The composition of Uranus's atmosphere is different from its bulk, consisting mainly of molecular
+                hydrogen and helium. The helium molar fraction, i.e. the number of helium atoms per molecule of gas, is
+                0.15±0.03 in the upper troposphere.</p>
+            <br>`
+}
+
+
+
+
+// -----------------------------...........NEPTUNE........-----------------------------------
+
+
+
+// neptune_button_1.onclick = neptune_txt_1;
+
+function neptune_txt_1() {
+    document.getElementById('planet_image').src = 'neptune.png'
+
+    document.getElementById('neptune_text').innerHTML = `<h1>NEPTUNE</h1>
+                <br>
+                <p>Neptune is the eighth and farthest-known Solar planet from the Sun. It is the
+                    fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It
+                    is 17
+                    times the mass of Earth, more massive than its near-twin Uranus.</p>
+                <br>`
+    // console.log("clicked");
+}
+// neptune_button_2.onclick = neptune_txt_2;
+
+function neptune_txt_2() {
+    document.getElementById('planet_image').src = 'neptune-2.png'
+
+    document.getElementById('neptune_text').innerHTML = `<h1>NEPTUNE</h1>
+            <br>
+            <p>Neptune's internal structure resembles that of Uranus. Its atmosphere forms about 5% to 10% of its mass
+                and extends perhaps 10% to 20% towards the core. Increasing concentrations of methane,
+                ammonia and water are found in the lower regions.</p>
+            <br>`
+}
+
+// neptune_button_3.onclick = neptune_txt_3;
+
+function neptune_txt_3() {
+    document.getElementById('planet_image').src = 'neptune-4.png'
+
+    document.getElementById('neptune_text').innerHTML = `<h1>NEPTUNE</h1>
+            <br>
+            <p>Neptune's atmosphere is 80% hydrogen and 19% helium. A trace amount of methane is also present. Prominent
+                absorption bands of methane exist at wavelengths above 600 nm, in the red and infrared portion of the
+                spectrum.</p>
+            <br>`
+}
